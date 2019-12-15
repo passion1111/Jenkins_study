e9fe902ea6200c94483903dae10fc2c9ac529342
passion1111 <se0572@naver.com>
2019-12-15 09:12:44 +0900

    test

--- src/main/java/test/test/test/HomeController.java
+++ src/main/java/test/test/test/HomeController.java
@@ -29,7 +29,7 @@ public class HomeController {
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
 		
-		String formattedDate = dateFormat.format(date)+"test13";
+		String formattedDate = dateFormat.format(date)+"test14";
 		
 		model.addAttribute("serverTime", formattedDate );
 		
